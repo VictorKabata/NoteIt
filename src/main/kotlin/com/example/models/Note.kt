329b3afc8eb0903f9084f179917eac6f79f61945
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-04-24 22:09:00 +0300

    Set serialized name

--- src/main/kotlin/com/example/models/Note.kt
+++ src/main/kotlin/com/example/models/Note.kt
@@ -1,13 +1,14 @@
 package com.example.models
 
 import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Note(
-    @Expose val id: Int,
-    @Expose val title: String,
-    @Expose val description: String,
-    @Expose val createdAt: String,
-    @Expose val updatedAt: String? = null
+    @Expose @SerializedName("id") val id: Int,
+    @Expose @SerializedName("title") val title: String,
+    @Expose @SerializedName("description") val description: String,
+    @Expose @SerializedName("created_at") val createdAt: String,
+    @Expose @SerializedName("updated_at") val updatedAt: String? = null
 )
--- src/main/kotlin/com/example/models/User.kt
+++ src/main/kotlin/com/example/models/User.kt
@@ -1,14 +1,15 @@
 package com.example.models
 
 import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 import io.ktor.server.auth.*
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class User(
-    @Expose var email: String,
+    @Expose @SerializedName("email") var email: String,
 
-    @Expose var userName: String,
+    @Expose @SerializedName("user_name") var userName: String,
 
     @Expose(serialize = false) var hashPassword: String
 ) : Principal
