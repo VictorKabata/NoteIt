1e9179600b3089e4d27e5b2cd36e5194c9de7a6c
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-04-28 17:50:42 +0300

    Set up github action workflow

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,8 +1,12 @@
 package com.example
 
 import com.example.cache.DatabaseFactory.initDatabase
-import io.ktor.server.application.*
-import io.ktor.server.netty.*
+import com.example.plugins.configureMonitoring
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSecurity
+import com.example.plugins.configureSerialization
+import io.ktor.server.application.Application
+import io.ktor.server.netty.EngineMain
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
