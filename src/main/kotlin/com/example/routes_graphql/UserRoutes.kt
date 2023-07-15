9c4797ca826962f979d804e9e024a2f71fc16aa5
Victor Kabata <victorbro14@gmail.com>
2023-07-15 05:25:33 +0300

    Update UserRoutes.kt

--- src/main/kotlin/com/example/routes_graphql/UserRoutes.kt
+++ src/main/kotlin/com/example/routes_graphql/UserRoutes.kt
@@ -5,21 +5,42 @@ import com.example.authentication.JwtService.generateToken
 import com.example.authentication.JwtService.hash
 import com.example.models.User
 import com.example.repository.UserRepository
-import com.example.utils.Constants
-import com.example.utils.ResponseHandler.errorResponse
-import com.example.utils.ResponseHandler.successResponse
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.call
-import io.ktor.server.auth.authenticate
-import io.ktor.server.auth.principal
-import io.ktor.server.request.receive
-import io.ktor.server.routing.Route
-import io.ktor.server.routing.delete
-import io.ktor.server.routing.get
-import io.ktor.server.routing.post
-import io.ktor.server.routing.put
-import io.ktor.server.routing.route
 
-fun SchemaBuilder.userGraphQLRoutes(){
+fun SchemaBuilder.userGraphQLRoutes(userRepository: UserRepository = UserRepository()) {
+
+    query("user") {
+        description = "Get user info"
+        resolver { email: String -> userRepository.getUser(email = email) }
+    }
+
+    mutation("register_user") {
+        description = "Register a new user"
+        resolver { user: User -> userRepository.createUser(user = user) }
+    }
+
+    query("login") {
+        description = "Generate a user auth token"
+        resolver { email: String, password: String ->
+            val user = userRepository.getUser(email = email)
+
+            return@resolver if (user != null && user.hashPassword == password.hash()) {
+                user.generateToken()
+            } else null
+        }
+    }
+
+    mutation("update_user") {
+        description = "Update existing user"
+        resolver { email: String, user: User ->
+            userRepository.updateUser(user = user, email = email)
+        }
+    }
+
+    mutation("delete_user") {
+        description = "Delete existing user"
+        resolver { email: String ->
+            userRepository.deleteUser(email = email)
+        }
+    }
 
 }
