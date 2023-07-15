436082eae1da88d1882817f1dec2ab0f3599feaa
Victor Kabata <victorbro14@gmail.com>
2023-07-15 04:52:20 +0300

    Create HomeRoutes.kt

---
+++ src/main/kotlin/com/example/routes_graphql/HomeRoutes.kt
@@ -0,0 +1,11 @@
+package com.example.routes_graphql
+
+import com.apurebase.kgraphql.schema.dsl.SchemaBuilder
+
+fun SchemaBuilder.homeGraphQLRoutes() {
+    query("home") {
+        description = "Home endpoint"
+        resolver { -> mapOf("message" to "Hello, World!") }
+    }
+
+}
